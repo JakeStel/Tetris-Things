f138ea431abd105645fee2a7f622271cc82f5028
JakeStel <jakestelly0924@gmail.com>
2021-05-11 14:18:26 -0500

    -Score and levels added

--- Tetris v3.py
+++ Tetris v3.py
@@ -1,12 +1,12 @@
 from tkinter import *
 import random
 from matrix_rotation import rotate_array as ra
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 
 
-buttons = [18,19,20,21]
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(buttons, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+#buttons = [18,19,20,21,22]
+#GPIO.setmode(GPIO.BCM)
+#GPIO.setup(buttons, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 
 DEBUG = False
 
@@ -18,6 +18,7 @@ class Shape:
         self.row = row
         self.col = column
         self.coords = coords
+        self.rotation_index = 0
 
 #===Main Class===#
 class Tetris:
@@ -35,10 +36,14 @@ class Tetris:
         self.canvas.grid(row=0, column = 0)
         self.startLine = self.canvas.create_line(0, self.h/6, self.w, self.h/6, width = 2)
         self.R_border = self.canvas.create_line(self.w, 0, self.w, self.h, width = 2)
+        self.L_border = self.canvas.create_line(3,0,3,self.h, width = 2)
+        self.B_border = self.canvas.create_line(0, self.h, self.w, self.h, width = 2)
         self.tickrate = 1000
         self.spawning = False
         self.master.after(self.tickrate, self.fps)
         self.active_piece_state = False
+        self.Score = 0
+        self.LvL = 0
 
         #=Creating the Shapes=#
         self.Pool = ['s','z','r','L','o','I','T']
@@ -81,14 +86,16 @@ class Tetris:
 
         #=KeyBinds=#
         self.master.bind('<Down>', self.move)
-        GPIO.add_event_detect(buttons[0], GPIO.RISING, callback = self.move)
+ #       GPIO.add_event_detect(buttons[0], GPIO.RISING, callback = self.move)
         self.master.bind('<q>', self.rotation)
-        GPIO.add_event_detect(buttons[1], GPIO.RISING, callback = self.move)
+ #       GPIO.add_event_detect(buttons[1], GPIO.RISING, callback = self.rotation)
         self.master.bind('<e>', self.rotation)
+ #       GPIO.add_event_detect(buttons[4], GPIO.RISING, callback = self.rotation)
         self.master.bind('<Left>', self.move)
-        GPIO.add_event_detect(buttons[2], GPIO.RISING, callback = self.move)
+ #       GPIO.add_event_detect(buttons[2], GPIO.RISING, callback = self.move)
         self.master.bind('<Right>', self.move)
-        GPIO.add_event_detect(buttons[3], GPIO.RISING, callback = self.move)
+ #       GPIO.add_event_detect(buttons[3], GPIO.RISING, callback = self.move)
+
         
 
                        
@@ -103,6 +110,35 @@ class Tetris:
             self.spawning = not self.spawning
             
         self.move()
+        
+        if (self.Score < 5):
+            self.tickrate = 1000
+            self.LvL = 1
+        elif (self.Score < 10):
+            self.tickrate = 750
+            self.LvL = 2
+        elif (self.Score < 15):
+            self.tickrate = 500
+            self.LvL = 3
+        elif (self.Score < 20):
+            self.tickrate = 250
+            self.LvL = 4
+        elif (self.Score >= 20):
+            self.tickrate = 100
+            self.LvL = 5
+            
+        self.score_label = Label(self.master,
+                                    text='Score {}'.format(self.Score),
+                                    height=5,
+                                    font=('Arial Black', 12))
+        self.score_label.grid(row=0, column=1,sticky = 'N')
+
+        self.LvL_label = Label(self.master,
+                                    text= 'Level {}'.format(self.LvL),
+                                    height=5,
+                                    font=('Arial Black', 12))
+        self.LvL_label.grid(row=0, column=2,sticky = 'N')
+
         
         self.master.after(self.tickrate, self.fps)
         
@@ -221,13 +257,13 @@ class Tetris:
             self.active_piece.coords[coords_idx] = x1,y1,x2,y2
             self.canvas.coords(id, self.active_piece.coords[coords_idx])
         #print(self.active_piece)
-#====================Check&Move Function====================#
-    def check_and_move(self, r, c):
-        pass
-        
+            
 #====================Rotation Function====================#
     def rotation(self, event=None):
         if not self.active_piece:
+            return
+        if len(self.active_piece.shape) == len(self.active_piece.shape[0]):
+            self.active_piece.rotation_index = self.active_piece.rotation_index
             return
         r = self.active_piece.row
         c = self.active_piece.col
@@ -239,33 +275,36 @@ class Tetris:
         if direction == 'q':
             direction = 'left'
             shape = ra(self.active_piece.shape, -90)
+            rotation_index = (self.active_piece.rotation_index - 1)%4
+            rx,ry = self.active_piece.rotation[rotation_index]
+            rotation_offsets = -rx,-ry
+            
         elif direction == 'e':
             direction = 'right'
             shape = ra(self.active_piece.shape, 90)
+            rotation_index = self.active_piece.rotation_index
+            rotation_offsets = self.active_piece.rotation[rotation_index]
+            rotation_index = (rotation_index + 1)%4
 
         l = len(shape) #rotated length
         w = len(shape[0]) #rotated width
         rt= y - l//2 #rotated row
         ct= x - w//2  #rotated column
-        if w%2:
-            if direction == 'right':
-                ct += 1
-            elif direction == 'left':
-                ct -= 1
-        if l%2:
-            if direction == 'right':
-                rt += 1
-            elif direction == 'left':
-                rt -= 1
-            
+        x_adjust,y_adjust = rotation_offsets
+        rt += y_adjust
+        ct += x_adjust
+
+
+        print(shape)
         for row, blocks in zip(range(rt, rt+l),
                                shape
                                ):
+            for column, block in zip(range(ct,ct+w), blocks):
                 
-                for column, block in zip(range(ct,ct+w), blocks):
-                    if block and self.grid[row][column] == 'x':
-                        return
-
+                if (column not in range(self.grid_width)
+                    or (row not in range(self.grid_height))
+                    or (blocks and self.grid[row][column] == 'x')): # also, make sure it's on the board
+                    return
         self.active_piece.shape = shape
 
         x_start = min(coord for tup in self.active_piece.coords for coord in (tup[0], tup[2]))
@@ -291,10 +330,11 @@ class Tetris:
             
 
 
-
+#====================bottom Function===================#
     def bottom(self, r):
         #Checks for loss by height
         self.spawning = not self.spawning
+        self.bottom_check = True
         if DEBUG:
             print("Piece has reached the bottom")
             for row in self.grid:
@@ -307,11 +347,12 @@ class Tetris:
             row[:] = ['x' if cell == '*' else cell for cell in row]
         for (x1,y1,x2,y2),id in zip(self.active_piece.coords, self.active_piece.piece):
             self.field[y1//self.square_w][x1//self.square_w] = id
-        indices = [idx for idx, row in enumerate(self.grid) if all(row)]
+            indices = [idx for idx, row in enumerate(self.grid) if all(row)]
         if indices:
+            self.Score += len(indices)
             self.remove_line(indices)
         
-
+#====================add_piece Function===================#
     def add_piece(self):
         if DEBUG:
             print("Creating random piece")
@@ -325,6 +366,18 @@ class Tetris:
         w = len(shape[0])
         startUp = (10-w)//2
         self.active_piece = Shape(shape,[], 0, startUp, [])
+        if 3 in (len(shape), len(shape[0])):
+            self.active_piece.rotation = [(0,0),
+                                          (1,0),
+                                          (-1,1),
+                                          (0,-1)]
+        else:
+            self.active_piece.rotation = [(1,-1),
+                                          (0,1),
+                                          (0,0),
+                                          (-1,0)]
+        if len(shape) < len(shape[0]): # wide shape
+            self.active_piece.rotation_index += 1
         for y,row in enumerate(shape):
             self.grid[y][startUp:startUp+w] = shape[y]
             for x,grid_cell in  enumerate(row, start=startUp):
@@ -342,15 +395,18 @@ class Tetris:
                                                      fill=self.color[Chossen], width=2))
 
 
+#====================loss Function===================#
     def loss(self):
         pass
 
+#====================remove_line Function===================#
     def remove_line(self, indices):
         for idx in indices:
             self.grid.pop(idx)
             self.grid.insert(0, ['' for column in range(self.grid_width)])
         self.Sweep(indices)
 
+#====================Sweep Function===================#
     def Sweep(self, indices,current_column=0):
         for row in indices:
             id = self.field[row][current_column]
